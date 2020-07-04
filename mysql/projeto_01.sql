008e592d845f53246ad16a5bfae8ad5932833ba1
Diego Menezes <depoiseuleio@gmail.com>
2020-07-04 17:05:26 -0300

    banco de dados

---
+++ mysql/projeto_01.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Jul-2020 às 22:04
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto_01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.online`
+--
+
+CREATE TABLE `tb_admin.online` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `ultima_acao` datetime NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.usuarios`
+--
+
+CREATE TABLE `tb_admin.usuarios` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cargo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_admin.usuarios`
+--
+
+INSERT INTO `tb_admin.usuarios` (`id`, `user`, `password`, `img`, `nome`, `cargo`) VALUES
+(1, 'admin', 'admin', 'gisele.jpg', 'Gisele Bezerra', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.visitas`
+--
+
+CREATE TABLE `tb_admin.visitas` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `dia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_admin.usuarios`
+--
+ALTER TABLE `tb_admin.usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.online`
+--
+ALTER TABLE `tb_admin.online`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.usuarios`
+--
+ALTER TABLE `tb_admin.usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.visitas`
+--
+ALTER TABLE `tb_admin.visitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
